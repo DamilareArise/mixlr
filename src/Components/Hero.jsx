f8c7ba67f4f45d2023a39b8d23c67157496c4cdf
Razzle <oseniabdulhameed001@gmail.com>
2024-09-11 13:32:19 +0100

    second

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <>
+     <div className="first">
+        <h1>Get your audio to your audience</h1>
+        <p>Professional tools for audio, made simple</p>
+        <button type="text"><a href="">Start free trial</a></button>
+    </div>
+    </>
+  )
+}
+
+export default Hero
